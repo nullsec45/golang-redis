206e889305d409c49952eac0c1b0f4eb5504758c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-06 10:29:14 +0700

    feat : list, set, sorted set, hash

--- redis_test.go
+++ redis_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 var client=redis.NewClient(&redis.Options{
-	Addr:"172.23.0.3:6379",
+	Addr:"172.23.0.2:6379",
 	DB:0,
 })
 
@@ -38,4 +38,52 @@ func TestString(t *testing.T) {
 	time.Sleep(time.Second * 5)
 	result, err = client.Get(ctx, "name").Result()
 	assert.NotNil(t, err)
+}
+
+func TestList(t *testing.T) {
+	client.RPush(ctx,"names","Rama")
+	client.RPush(ctx,"names","Fajar")
+
+	assert.Equal(t, "Rama", client.LPop(ctx,"names").Val())
+	assert.Equal(t,"Fajar",client.LPop(ctx,"names").Val())
+
+	client.Del(ctx,"names")
+}
+
+func TestSet(t *testing.T) {
+	client.SAdd(ctx, "students", "Rama")
+	client.SAdd(ctx, "students", "Rama")
+	client.SAdd(ctx, "students", "Fajar")
+	client.SAdd(ctx, "students", "Fajar")
+
+	assert.Equal(t, int64(2), client.SCard(ctx,"students").Val())
+	assert.Equal(t, []string{"Rama","Fajar"}, client.SMembers(ctx,"students").Val())
+}
+
+func TestSortedSet(t *testing.T) {
+	client.ZAdd(ctx, "scores", redis.Z{Score:100, Member:"Fajar"})
+	client.ZAdd(ctx, "scores", redis.Z{Score:85, Member:"Feast"})
+	client.ZAdd(ctx, "scores", redis.Z{Score:65, Member:"Rama"})
+	client.ZAdd(ctx, "scores", redis.Z{Score:95, Member:"Hindia"})
+	client.ZAdd(ctx, "scores", redis.Z{Score:80, Member:"Bernadya"})
+
+	assert.Equal(t,[]string{"Rama","Bernadya","Feast","Hindia","Fajar"}, client.ZRange(ctx,"scores", 0,4).Val())
+	assert.Equal(t,"Fajar",client.ZPopMax(ctx,"scores").Val()[0].Member)
+	assert.Equal(t,"Hindia",client.ZPopMax(ctx,"scores").Val()[0].Member)
+	assert.Equal(t,"Feast",client.ZPopMax(ctx,"scores").Val()[0].Member)
+	assert.Equal(t,"Bernadya",client.ZPopMax(ctx,"scores").Val()[0].Member)
+	assert.Equal(t,"Rama",client.ZPopMax(ctx,"scores").Val()[0].Member)
+}
+
+func TestHash(t *testing.T) {
+	client.HSet(ctx, "user:1","id","1")
+	client.HSet(ctx, "user:1","name","Fajar")
+	client.HSet(ctx, "user:1","email","fajar@example.com")
+
+	user := client.HGetAll(ctx, "user:1").Val()
+	assert.Equal(t, "1", user["id"])
+	assert.Equal(t, "Fajar", user["name"])
+	assert.Equal(t,"fajar@example.com",user["email"])
+
+	client.Del(ctx,"user:1")
 }
